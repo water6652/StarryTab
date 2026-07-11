d2097fa4ad27a6f9e7dfe46c2c2b217d000f24c6
water6652 <abd24.bgr@gmail.com>
2026-07-11 19:43:16 +0100

    fixed whatever is wrong with git on my machine

---
+++ src/Searchengines.js
@@ -0,0 +1,9 @@
+export const SEARCH_ENGINES = {
+    google: {
+        label: "Google (Default)"
+        template: "https://www.google.com/search?q=%s"
+    },
+    duckduckgo: {
+        label: "DuckDuckGo",
+        template: "https://duckduckgo.com/?q=%s",
+    },
